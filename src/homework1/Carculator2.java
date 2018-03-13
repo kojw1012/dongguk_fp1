e243243ae2befd693ff014b4b7c696eda5c21834
ko jae woong <rhwodnd11@gmail.com>
2018-03-13 10:49:10 +0900

    Homework1

---
+++ src/homework1/Carculator2.java
@@ -0,0 +1,7 @@
+public class Carculator2 {
+    public static void main(String []args) {
+        System.out.printf("%d+%d=%d\n",1,2,1+2);
+        System.out.printf("%d-%d=%d\n",1,2,1-2);
+        System.out.printf("%d*%d=%d\n",1,2,1*2);
+    }
+}
