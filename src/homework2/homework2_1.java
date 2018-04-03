005811faec313ddc34f227897426207fc65f7c6f
jae woong ko <rhwodnd11@gmail.com>
2018-04-04 01:49:08 +0900

    'homework2_1.java'

---
+++ src/homework2/homework2_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class homework2_1 {
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Input your number:");
+        int a= sc.nextInt();
+        byte v =(byte)a;
+
+        System.out.printf("%s는 %d를 2진수로 표현한 것입니다.", toBinaryString(v),a);
+    }
+    static String toBinaryString(byte v){
+        String zero = "00000000";
+        String tmp= zero+Integer.toBinaryString(v);
+        return tmp.substring(tmp.length() -8);
+    }
+}
